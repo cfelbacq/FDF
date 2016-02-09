ac9a717357a793ae91812bb206426f507038cff3
Cyril FELBACQ <cfelbacq@e1r8p15.42.fr>
2016-02-09 17:02:19 +0100

    09/02/2016

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cfelbacq <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 12:56:47 by cfelbacq          #+#    #+#             */
-/*   Updated: 2016/02/07 17:06:26 by cfelbacq         ###   ########.fr       */
+/*   Updated: 2016/02/09 16:58:00 by cfelbacq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <math.h>
 
 void	place_point(int **integer_tab, t_env *data, t_win *window);
+void	free_draw(t_env *data);
 
 int		expose_hook(t_env *data)
 {
@@ -22,103 +23,85 @@ int		expose_hook(t_env *data)
 	window.width = data->win_width;
 	window.height = data->win_height;
 
+	free_draw(data);
 	place_point(data->integer_tab_s, data, &window);
-	//t_pos destination;
-	//mlx_pixel_put(data->mlx, data->win, start.x, start.y, data->color);
-	//draw(data, &start, &destination);
 	return (0);
+}
+
+void	free_draw(t_env *data)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < data->win_height)
+	{
+		j = 0;
+		while (j < data->win_width)
+		{
+			mlx_pixel_put(data->mlx, data->win, j, i, 0x000000);
+			j++;
+		}
+		i++;
+	}
 }
 
 int		key_hook(int keycode, t_env *data)
 {
 	if (keycode == 53)
-	{
 		exit(0);
-	}
 	return (0);
 }
 
-
-void	draw_x(t_env *data, int **integer_tab, int addition_x, int addition_y,\
-		t_win *window)
+t_pos	threed(int i, int j, t_pos a, int z, t_pos *add)
 {
-	int i;
-	int j;
-	t_pos a;
-	t_pos b;
-
-	i = 1;
-	while (i < data->max_y + 1)
-	{
-		j = 1;
-		while (j < data->max_x + 1)
-		{
-			a.x = ((M_SQRT2 / 2) * (j - i)) * addition_x;
-			a.y = -(((-0.37) * (j + i)) + 0.60 + integer_tab[i - 1][j - 1]) * addition_y;
-			a.x *= 0.5;
-			a.y *= 0.5;
-			a.x += 500;
-			a.y += 500;
-			ft_putstr("draw_x a.x : ");
-			ft_putnbr(a.x);
-			ft_putchar('\n');
-			ft_putstr("draw_x a.y : ");
-			ft_putnbr(a.y);
-			ft_putchar('\n');
-			if (j > 1)
-				draw (data, &b, &a);
-			b = a;
-			j++;
-		}
-		i++;
-	}
+	a.x = ((M_SQRT2 / 2) * (j - i)) * add->x;
+	a.y = -(((-0.60) * (j + i)) + 0.82 + z)\
+		   * add->y;
+	a.x *= 0.5;
+	a.y *= 0.5;
+	a.x += 500;
+	a.y += 500;
+	return (a);
 }
 
-void	draw_y(t_env *data, int **integer_tab, int addition_x, int addition_y,\
-		t_win *window)
+void	draw_x(t_env *data, int **integer_tab, t_pos *add, t_win *window)
 {
-	int i;
-	int j;
+	t_pos i;
 	t_pos a;
 	t_pos b;
 
-	i = 1;
-	while (i < data->max_x + 1)
+	i.y = 1;
+	while (i.y < data->max_y + 1)
 	{
-		j = 1;
-		while (j < data->max_y + 1)
+		i.x = 1;
+		while (i.x < data->max_x + 1)
 		{
-			a.x = ((M_SQRT2 / 2) * (i - j)) * addition_x;
-			a.y = -(((-0.37) * (i + j)) + 0.60 + integer_tab[j - 1][i - 1]) * addition_y;
-			a.x *= 0.5;
-			a.y *= 0.5;
-			a.x += 500;
-			a.y += 500;
-			ft_putstr("draw_y a.x : ");
-			ft_putnbr(a.x);
-			ft_putchar('\n');
-			ft_putstr("draw_y a.y : ");
-			ft_putnbr(a.y);
-			ft_putchar('\n');
-			if (j > 1)
+			a = threed(i.y, i.x, a, integer_tab[i.y - 1][i.x - 1], add);
+			if (i.x > 1)
+			{
+				b = threed(i.y, i.x - 1, a, integer_tab[i.y - 1][i.x - 2], add);
 				draw (data, &b, &a);
-			b = a;
-			j++;
+			}
+			if (i.y > 1)
+			{
+				b = threed(i.y - 1, i.x, a, integer_tab[i.y - 2][i.x - 1], add);
+				draw(data, &b, &a);
+			}
+			i.x++;
 		}
-		i++;
+		i.y++;
 	}
 }
 
 void	place_point(int **integer_tab, t_env *data, t_win *window)
 {
-	int addition_x;
-	int addition_y;
+	t_pos add;
 
 	data->color = 0xffffff;
-	addition_x = (window->width - 100) / (data->max_x + 1);
-	addition_y = (window->height - 100) / (data->max_y + 1) ;
-	draw_x(data, integer_tab, addition_x, addition_y, window);
-	draw_y(data, integer_tab, addition_x, addition_y, window);
+	add.x = (window->width - 100) / (data->max_x + 1);
+	add.y = (window->height - 100) / (data->max_y + 1) ;
+	draw_x(data, integer_tab, &add, window);
 }
 
 void	find_max_int(int **integer_tab, t_env *data)
@@ -172,7 +155,7 @@ int	main(int argc, char **argv)
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, window.height, window.width, "42");
 	find_max_int(integer_tab, &data);
-	//place_point(integer_tab, &data, &window);
+	place_point(integer_tab, &data, &window);
 	data.win_width = window.width;
 	data.win_height = window.height;
 	data.integer_tab_s = integer_tab;
